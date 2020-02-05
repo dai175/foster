fb35b0062b16e96aa251db119bafa782db2f3d24
Daisuke Ohba <dai175dai@gmail.com>
2020-02-06 08:27:37 +0900

    Add function of authorization

--- config.py
+++ config.py
@@ -7,12 +7,14 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 UPLOAD_FOLDER = 'static/images'
 
+AUTH0_DOMAIN = 'foster-an-animal.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'foster'
+CLIENT_ID = '4l2ZyAy8tb0i85R5CT32MaiUUt5dE0iD'
+CALLBACK_URI = 'http://localhost:8080/login-results'
+
 LOGIN_URI = 'https://{}/authorize?audience={}' \
             '&response_type=token&client_id={}&redirect_uri={}'. \
-    format('foster-an-animal.auth0.com',
-           'foster',
-           '4l2ZyAy8tb0i85R5CT32MaiUUt5dE0iD',
-           'http://localhost:8080/login-results'
-           )
+    format(AUTH0_DOMAIN, API_AUDIENCE, CLIENT_ID, CALLBACK_URI)
 
 LOGOUT_URI = 'http://localhost:8080/logout'
