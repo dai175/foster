94d9e183fd844f615bf36e3e5b97f4ba43796c6b
Daisuke Ohba <dai175dai@gmail.com>
2020-02-15 22:07:28 +0900

    Modify config.py for heroku

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 SECRET_KEY = os.urandom(32)
 
-SQLALCHEMY_DATABASE_URI = 'postgres://user:password@localhost:5432/foster'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or\
+                          'postgres://user:password@localhost:5432/foster'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 UPLOAD_FOLDER = 'static/images'
@@ -11,7 +12,7 @@ AUTH0_DOMAIN = 'foster-an-animal.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'foster'
 CLIENT_ID = '4l2ZyAy8tb0i85R5CT32MaiUUt5dE0iD'
-CALLBACK_URI = 'http://localhost:5000/callback'
+CALLBACK_URI = 'https://foster-an-animal.herokuapp.com/callback'
 
 LOGIN_URI = 'https://{}/authorize?audience={}' \
             '&response_type=token&client_id={}&redirect_uri={}'. \
